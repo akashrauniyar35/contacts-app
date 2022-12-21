8a19f9986ddcd09b32c6bfb44c21f83c51693463
Akash Rouniyar <akashrouniyar@192-168-1-114.tpgi.com.au>
2022-12-21 22:06:14 +1100

    firebase CRUDE setup

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -1,120 +1,92 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import dataInterface from '../interfaces/dataInterface'
 import SearchBar from '../searchBar/SearchBar'
 import AddContacts from './AddContacts'
-import './Contacts.css'
 import ContactsCard from './ContactsCard'
+import './Contacts.css'
+import Spinner from '../spinner/Spinner'
 
+import { deleteContact, fetchAllContacts, postContact, updateContact } from '../config/contactServices'
 
-const data: dataInterface = [
-    {
-        id: '00',
-        name: 'Millan Adhikari',
-        number: "0415701345",
-        email: 'testOne@gmail.com',
-
-    },
-    {
-        id: '01',
-        name: 'Samsung Test',
-        number: "1",
-        email: 'testTwo@gmail.com',
-
-    },
-    {
-        id: '02',
-        name: 'Apple Ball',
-        number: "2",
-        email: 'testTwo@gmail.com',
-
-    },
-    {
-        id: '03',
-        name: 'Sample Man',
-        number: "12",
-        email: 'testTwo@gmail.com',
-    },
-    {
-        id: '03',
-        name: 'Sample Man',
-        number: "12",
-        email: 'testTwo@gmail.com',
-    },
-    {
-        id: '03',
-        name: 'Sample Man',
-        number: "12",
-        email: 'testTwo@gmail.com',
-    },
-    {
-        id: '03',
-        name: 'Sample Man',
-        number: "12",
-        email: 'testTwo@gmail.com',
-    },
-    {
-        id: '03',
-        name: 'Sample Man',
-        number: "12",
-        email: 'testTwo@gmail.com',
-    },
-    {
-        id: '03',
-        name: 'Sample Man',
-        number: "12",
-        email: 'testTwo@gmail.com',
-    },
-    {
-        id: '03',
-        name: 'Sample Man',
-        number: "12",
-        email: 'testTwo@gmail.com',
-    },
-    {
-        id: '03',
-        name: 'Sample Man',
-        number: "12",
-        email: 'testTwo@gmail.com',
-    },
-]
 
 
 function Contacts() {
+    const [loading, setLoading] = useState(false)
     const [addModalVisible, setAddModalVisible] = useState<Boolean>(false)
+    const [editModalVisible, setEditModalVisible] = useState<Boolean>(false)
+    const [searchValue, setSearchValue] = useState(String)
     const [filtersVisible, setFiltersVisible] = useState<Boolean>(false)
-    const [editObj, setEditObj] = useState()
 
+    const [data, setData] = useState([])
+
+    const [editObj, setEditObj] = useState()
 
 
     const addModalToggler = () => {
         setAddModalVisible(!addModalVisible)
         console.log("modal", addModalVisible)
     }
-    const editHandler = (item: any) => {
-        setAddModalVisible(!addModalVisible)
+    const editModalToggler = (item: any) => {
+        setEditModalVisible(!editModalVisible)
         setEditObj(item)
     }
 
-    const onSaveHandler = () => {
+    const onAddContactsHandler = (obj: any) => {
+        postContact(obj)
+        setAddModalVisible(false)
+        getContacts()
+    }
+    const onUpdateContactsHandler = (obj: any) => {
+        const id = obj.id
+        console.log("contacts update pressed", id)
+        updateContact(id, obj)
+        setEditModalVisible(false)
+        getContacts()
+    }
+    const onDeleteContactsHandler = (id: String) => {
+        deleteContact(id)
+        getContacts()
+    }
 
+    const searchHandler = (value: String) => {
+        let obj = data?.find((o: any) => o.name === value);
+        console.log('value', obj)
+    }
+
+    useEffect(() => {
+        getContacts()
+    }, []);
+
+    const getContacts = async () => {
+        setLoading(true)
+        const x: any = await fetchAllContacts()
+        setData(x.docs.map((doc: any) => ({ ...doc.data(), id: doc.id })))
+        setLoading(data && false)
     }
 
     return (
         <>
             <div className='contacts__container'>
-                <SearchBar addModalToggler={addModalToggler} />
-                {addModalVisible && <AddContacts obj={editObj} addModalToggler={addModalToggler} onSaveHandler={onSaveHandler} />}
+                <SearchBar value={searchValue} addModalToggler={addModalToggler} onSearch={searchHandler} />
                 <p>Contacts</p>
+
+                <div className="spinner__center">
+                    {loading && <Spinner />}
+                </div>
 
                 <div className="list">
                     {data.map((item: any) => {
                         return (
-                            <ContactsCard key={item.id} name={item.name} number={item.number} email={item.email} onEditPress={() => editHandler(item)} />
+                            <ContactsCard key={item.id} name={item.name} number={item.number} email={item.email} onEditPress={() => editModalToggler(item)} onDeletePress={() => onDeleteContactsHandler(item.id)} />
                         )
                     })}
                 </div>
 
             </div>
+
+            {addModalVisible && <AddContacts lable={"Add Contact"} onClose={addModalToggler} onAddContacts={onAddContactsHandler} />}
+
+            {editModalVisible && <AddContacts lable={"Edit Contact"} obj={editObj} onClose={editModalToggler} onUpdateContacts={onUpdateContactsHandler} />}
         </>
     )
 }
--- src/components/interfaces/customerCardInterface.tsx
+++ src/components/interfaces/customerCardInterface.tsx
@@ -4,4 +4,5 @@ export default interface customerCardInterface {
     number: String
     email: String
     onEditPress: any
+    onDeletePress: any
 }
